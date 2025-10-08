ef38aa45bce4e9f85cf6d390e23dd7487b86912b
Simon Dumesny <simondumesny@gmail.com>
2025-10-08 21:10:11 +0000

    rewrote `track.dart`

---
+++ lib/track.py
@@ -0,0 +1,87 @@
+from abc import ABC
+from enum import Enum
+
+
+class LastFMEntity(ABC):
+    """
+    An abstract class that represent what LastFM "entities" (track, albums, artists...) have in common.
+    """
+
+    mbid: str
+    "The **MusicBrainz ID (MBID)** for this entity. This unique identifier comes from the [MusicBrainz database](https://musicbrainz.org/), an open-source music encyclopedia."
+
+    name: str
+
+    def __init__(self, mbid, name):
+        self.mbid = mbid
+        self.name = name
+
+    def __eq__(self, __value):
+        assert type(__value) is Track, "Only Track to Track comparison implemented atm"
+        return __value.mbid == self.mbid
+
+    def __str__(self):
+        return f"name: {self.name}, mbid: {self.mbid}"
+
+
+class Artist(LastFMEntity):
+    def __init__(self, mbid, name):
+        super().__init__(mbid, name)
+
+    @staticmethod
+    def createFromData(data: dict):
+        return Artist(data["artist"]["#text"], data["artist"]["mbid"])
+
+
+class Album(LastFMEntity):
+    def __init__(self, mbid, name):
+        super().__init__(mbid, name)
+
+    @staticmethod
+    def createFromData(data: dict):
+        return Album(data["album"]["mbid"], data["album"]["name"])
+
+
+class ImageSize(Enum):
+    small = 1
+    medium = 2
+    large = 3
+    extraLarge = 4
+
+
+class Image:
+    imageSize: ImageSize
+    url: str
+
+    def __init__(self, imageSize: ImageSize, url):
+        self.imageSize = imageSize
+        self.url = url
+
+    def __str__(self):
+        return f"url: {self.url}, size: {self.imageSize}"
+
+
+class Track(LastFMEntity):
+    artist: Artist
+    album: Album
+    images: list[Image]
+    url: str
+
+    def __init__(self, name, mbid, artist: Artist, album: Album, images: list[Image]):
+        super().__init__(mbid, name)
+        self.artist = artist
+        self.album = album
+        self.images = images
+
+    @staticmethod
+    def createFromData(data: dict):
+        imageList = []
+        for image in data["images"]:
+            imageList.append(
+                Image(
+                    image["#text"],
+                    ImageSize.extraLarge
+                )
+            )
+
+        return Track(data["name"], data["mbid"], Artist.createFromData(data), Album.createFromData(data), imageList)
