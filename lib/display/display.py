67143c2ad14ad900e0cfa93e0fdfc7a96341048b
apoleon33 <simondumesny@gmail.com>
2026-04-11 21:59:32 +0200

    default placeholder image when fail to get color palette on second try

--- lib/display/display.py
+++ lib/display/display.py
@@ -16,14 +16,23 @@ class Display(ABC):
     def __init__(self, api: LastFM):
         self.api = api
 
-    def getColorPalette(self, imageUrl:str) -> Palette:
+    def getColorPalette(self, imageUrl:str, recur:bool = False) -> Palette:
         img = requests.get(imageUrl).content
         tempFile = open("temp.jpg", "wb")
 
         tempFile.write(img)
         tempFile.close()
 
-        palette = extract_colors(image='temp.jpg', palette_size=self.colorNumber)
+        try:
+            palette = extract_colors(image='temp.jpg', palette_size=self.colorNumber)
+        except Exception as e:
+            if recur:
+                # get placeholder image by default, might change that in config file tho
+                palette = extract_colors(image='assets/placeholder.jpeg', palette_size=self.colorNumber)
+                #raise Exception(f"while getting color palette for image '{imageUrl}' the following error occured: {e}")
+            else :
+                # retry one time
+                palette = self.getColorPalette(imageUrl, recur=True)
 
         os.remove("temp.jpg")
         return palette
