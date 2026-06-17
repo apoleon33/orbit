e99e9e24b2d93e652dc5fcdb2b424ed9d23ac713
Simon Dumesny <simondumesny@gmail.com>
2026-06-17 20:17:58 +0000

    functionnal dbus module

--- lib/display/dbus.py
+++ lib/display/dbus.py
@@ -8,33 +8,31 @@ from lib.track import ColoredTrack
 class DbusInterface(DbusInterfaceCommonAsync,
                     interface_name='org.apoleon.orbit'):
 
-    @dbus_signal_async(signal_signature='(sssssssssss)')
+    @dbus_signal_async(signal_signature='(ss)')
     def currentTrack(self) -> tuple:
         # name, mbid, artist, album, url, color1, color2, color3; color4, color5
         return NotImplementedError
+
+    @dbus_signal_async(signal_signature='i')
+    def hmmm(self) -> int:
+        # name, mbid, artist, album, url, color1, color2, color3; color4, color5
+        raise NotImplementedError
 
 
 class Dbus(RequestlessDisplay):
     exportObject = DbusInterface()
 
-    def __int__(self):
-        request_default_bus_name_async('org.apoleon.orbit')
+    async def init(self):
+        await request_default_bus_name_async('org.apoleon.orbit')
         self.exportObject.export_to_dbus('/')
 
     @staticmethod
     def convertTrackToDbus(track: ColoredTrack) -> tuple:
         return (
             track.name,
-            track.mbid,
-            track.artist,
-            track.album,
-            track.url,
-            track.palette.colors[0].hex,
-            track.palette.colors[1].hex,
-            track.palette.colors[2].hex,
-            track.palette.colors[3].hex,
-            track.palette.colors[4].hex
+            track.album.name
         )
 
     def show(self, track: ColoredTrack):
         self.exportObject.currentTrack.emit(Dbus.convertTrackToDbus(track))
+        self.exportObject.hmmm.emit(12)
